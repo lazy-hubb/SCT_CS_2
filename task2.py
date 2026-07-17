3e4f878fcf3a9b337e54fe5014a64b37d425d110
lazy-hubb <167401002+lazy-hubb@users.noreply.github.com>
2026-07-17 16:49:03 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+my_picture = Image.open("/home/juveriak/Desktop/test_images/input.jpg")
+width, height = my_picture.size
+pixels = my_picture.load()
+
+for x in range(width):
+    for y in range(height):
+        r, g, b = pixels[x, y]
+        pixels[x, y] = (r + 50, g - 50, b + 50)
+
+my_picture.save("/home/juveriak/Desktop/test_images/encrypted.jpg")
+print("✅ Image encrypted successfully! Check your Desktop.")
